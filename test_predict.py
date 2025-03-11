1614dbf79f7608e7e587e9f6d211c009034c27f5
Garrett Compston <compston.g@northeastern.edu>
2025-03-11 12:35:04 -0400

    individual file able to be tested with compute_accuracy

--- test_predict.py
+++ test_predict.py
@@ -4,7 +4,7 @@ import torch
 import pandas as pd
 import numpy as np
 
-from rfml.data import build_dataset, Dataset
+from rfml.data import build_dataset, DatasetBuilder
 from rfml.nn.eval import (
     compute_accuracy,
     compute_accuracy_on_cross_sections,
@@ -13,14 +13,14 @@ from rfml.nn.eval import (
 from rfml.nn.model import build_model
 from rfml.nn.train import build_trainer, PrintingTrainingListener
 
-# ADD ALL BELOW BACK WHEN DATA TYPE ISSUE RESOLVED
-
 train, val, test, le = build_dataset(dataset_name="RML2016.10a", path="/home/garrett/RML2016.10a/RML2016.10a_dict.pkl")
 
 model = build_model(model_name="CNN", input_samples=128, n_classes=len(le))
 
+
 print("Model built")
 
+# Trainer commented out for testing
 
 trainer = build_trainer(
     strategy="standard", max_epochs=3, gpu=True
@@ -28,18 +28,25 @@ trainer = build_trainer(
 trainer.register_listener(PrintingTrainingListener())
 trainer(model=model, training=train, validation=val, le=le)
 
-
+# Load in file
 file = np.fromfile(file="/home/garrett/PlutoSDR-20250304_142236-88_9MHz-2_1MSps-2_1MHz.complex", dtype=np.float32)
 
-print(file)
-tensor_from_file = torch.tensor(file) # convert file to be a tensor
+# Split file into 
+partitions = file.size / 256
+splitfile = np.split(file, partitions)
 
-model_pass = model(tensor_from_file) # pass the tensor through the model
-# above curretnly throwing errors for the format of the tensor
+builder = DatasetBuilder()
+
+for x in splitfile:
+    reshape_x = np.reshape(x, (2, 128))
+    builder.add(reshape_x, Modulation="WBFM")
 
 
-prediction = torch.argmax(model_pass, dim=1)
-print(prediction)
+dataset = builder.build()
+
+acc = compute_accuracy(model=model, data=dataset, le=le)
+print("Overall Testing Accuracy: {:.4f}".format(acc))
+
 print("Done")
 
 
