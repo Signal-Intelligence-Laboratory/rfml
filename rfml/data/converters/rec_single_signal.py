c8f5a16605954e6849adef49324591b79dd87427
Garrett Compston <compston.g@northeastern.edu>
2025-03-13 12:07:30 -0400

    file loading and proper conversion to how rfml treats iq data

--- rfml/data/converters/rec_single_signal.py
+++ rfml/data/converters/rec_single_signal.py
@@ -23,18 +23,51 @@ Note from editor: from what I've found in the URH code (urh/src/urh/signalproces
 # External Includes
 import numpy as np
 import os
-from warnings import warn
 
 # Internal Includes 
 from rfml.data import Dataset, DatasetBuilder
 
 class SingleSignalDataLoader(object):
-    def load(self, path: str):
+    """
+    Base load function, takes a filepath as a string, checks if the path exists, passes to the actual loader.
+    """
+    def load(self, path: str) -> np.ndarray:
         if path is not None:
             if not os.path.exists(path):
                 raise ValueError(
-                    "If path is provided, it must actually exist.  Provided path: "
-                    "{}".format(path)
+                    f"If path is provided, it must actually exist.  Provided path: {path}"
                 )
             return self.load_by_filetype(path=path)
+        
+    """
+    Accessed by load() function, passes back an NDArray of the data type given by the file extension.
+    """
+    def load_by_filetype(self, path: str) -> np.ndarray:
+        if path.endswith(".complex16u") or path.endswith(".cu8"):
+            return np.fromfile(path, dtype=np.uint8)
+        elif path.endswith(".complex16s") or path.endswith(".cs8"):
+            return np.fromfile(path, dtype=np.int8)
+        elif path.endswith(".complex32u") or path.endswith(".cu16"):
+            return np.fromfile(path, dtype=np.uint16).conver
+        elif path.endswith(".complex32s") or path.endswith(".cs16"):
+            return np.fromfile(path, dtype=np.int16)
+        else:
+            return np.fromfile(path, dtype=np.float32)
+
+    """
+    Takes in a 1-D NDArray[] of IQ samples and converts it to be 2-D with I and Q in separate arrays at the same index.
+    """
+    def ndarr_to_iq(self, arr: np.ndarray) -> np.ndarray:
+        if len(arr) % 2 == 0:
+            arr = arr.reshape((2, -1), order="C")
+        else:  # ignore the last half sample to avoid a conversion error
+            arr = arr[:-1].reshape((2, -1), order="C")
+        return arr
+
+    
+    # def partition(self, nsamps=int):
+
+    # for partition, use a modulo function to find number of samples to be cut off, and cut an even amount off the front and back
+
+
 
