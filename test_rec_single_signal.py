9a5fe63d47add6874990548ab80ba1aa8e2cf9e9
Garrett Compston <compston.g@northeastern.edu>
2025-03-14 10:10:24 -0400

    actually assigned the variable

--- test_rec_single_signal.py
+++ test_rec_single_signal.py
@@ -2,20 +2,13 @@ import numpy as np
 
 from rfml.data.converters.rec_single_signal import SingleSignalDataLoader
 
-x = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+arr = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
               [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]])
 
 sig = SingleSignalDataLoader()
 
-sig.partition(x, 4)
+arr = sig.partition(arr, 7)
 
+print(arr)
 
-
-
-
-
-
-
-
-print(x)
 
